7ba15e02de5e1bb8b9d1c94e86475f186a74f1f5
Sidi Yang <79249381+Stephen0808@users.noreply.github.com>
2024-07-12 22:04:38 +0800

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -19,7 +19,7 @@ parser.add_argument("--epoch", type=int, default=1)
 
 parser.add_argument("--num_epochs", type=int, default=400, help="last epoch of training (include)")
 parser.add_argument("--losses", type=str, nargs="+", default=["l1", "cos"], help="one or more loss functions")
-parser.add_argument("--model", type=str, nargs="+", default=["CLUTNet", "20+05+20"], help="model configuration, [n+s+w, dim]")
+parser.add_argument("--model", type=str, nargs="+", default=["ICELUT", "10+05+10"], help="model configuration, [n+s+w, dim]")
 # parser.add_argument("--model", type=str, nargs="+", default=['CLUTNet', '20+05+10'], help="model configuration, [nt, res, mlp, backbone]") 
 parser.add_argument("--name", type=str, help="name for this training (if None, use <model> instead)")
 
